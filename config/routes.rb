089ce5d153d4bad530bcc42bbd35f2f2c398f285
Josef Perry Tinagan <josephperry_06@yahoo.com>
2017-05-10 21:05:45 +0800

    added blogs controller, and its index page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "/home", to: 'static_pages#home'
   root to: 'static_pages#home'
+  resources :blogs, only: [:index]
 end
